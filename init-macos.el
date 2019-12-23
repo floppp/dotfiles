5d590097e3b55ea9de85eee45152221382d7f84e
floppp <altraciano@outlook.com>
2019-12-23 08:12:55 +0100

    Some extra comments and typo fixs.

--- init-macos.el
+++ init-macos.el
@@ -2,3 +2,17 @@
 (org-babel-load-file
  (expand-file-name "emacs-init.org"
                    user-emacs-directory))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("9129c2759b8ba8e8396fe92535449de3e7ba61fd34569a488dd64e80f5041c9f" default))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
