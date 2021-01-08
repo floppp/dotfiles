1871ea8698c35b44be80d403e68e5b914c99031e
Fernando López <ferlola@etsia.upv.es>
2021-01-08 21:28:10 +0100

    Chaning visual-regexp shorthucts

--- init.el
+++ init.el
@@ -206,7 +206,7 @@
 
 ;;; third-party packages
 (require 'visual-regexp)
-;; (define-key global-map (kbd "C-c r") 'vr/replace)
+(define-key global-map (kbd "C-c w") 'vr/replace)
 (define-key global-map (kbd "C-c q") 'vr/query-replace)
 ;; if you use multiple-cursors, this is for you:
 (define-key global-map (kbd "C-c m") 'vr/mc-mark)
